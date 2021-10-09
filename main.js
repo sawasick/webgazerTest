2f4afedc10137530a174ae5fd215c2f577e9d19e
sawasick <50393229+sawasick@users.noreply.github.com>
2021-10-10 01:23:02 +0900

    update: 赤を見続けると消えるように修正

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var count = 0;
 webgazer.setGazeListener(function(data, elapsedTime) {
     if (data == null) {
         return;
@@ -5,6 +6,12 @@ webgazer.setGazeListener(function(data, elapsedTime) {
     var xprediction = data.x;
     var yprediction = data.y;
     var z = document.elementFromPoint(xprediction, yprediction);
-    console.log(z);
+    // console.log(z);
+    if(z.attr('box3')) {
+      count++;
+    }
+    if(count >= 50) {
+      z.remove();
+    }
     // console.log(elapsedTime);
 }).begin();
